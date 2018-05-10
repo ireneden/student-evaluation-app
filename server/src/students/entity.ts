8ce2fdeaad9aa5ad2055e9e0c08ba45c955c8330
Irene de Nicolo <irene.denicolo89@gmail.com>
2018-05-10 18:32:12 +0200

    several changes

--- server/src/students/entity.ts
+++ server/src/students/entity.ts
@@ -18,6 +18,6 @@ export default class Student extends BaseEntity {
     @Column('text', { nullable: true })
     batchId: number
 
-    @Column('text', { nullable: true })
+    @Column('text', { nullable: true, default: 'White'  })
     latestEvaluation: string
 }
